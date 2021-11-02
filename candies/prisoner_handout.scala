32a6dab43e8f9f2e7d3cdd04349dcf1e3c5319de
Chad Crowe <ccrowe@ccrowe-mn3.linkedin.biz>
2021-11-02 07:17:19 -0700

    solved prisoner candies

--- candies/prisoner_handout.scala
+++ candies/prisoner_handout.scala
@@ -1,6 +1,26 @@
 import scala.collection.immutable.Queue
 
 class Candies {
+  def getLast(current: Int, size: Int): Int = {
+    if(current == 1){
+      return size
+    }else{
+      return current - 1
+    }
+  }
+  def feed(current: Int, size: Int): Int = {
+    if(current == size){
+      return 1
+    }else{
+      return current + 1
+    }
+  }
+  def feed2(queue: Queue[Int]): Queue[Int] = {
+    val removed = queue.dequeue
+    val item = removed._1
+    val updatedQueue = removed._2
+    updatedQueue.enqueue(item)
+  }
   def rotateDeque(n: Int, deque: Queue[Int]): Queue[Int] = {
     if (n <= 0) return deque
     var ad = deque
@@ -12,7 +32,26 @@ class Candies {
     }
     ad
   }
+  def fillQueue(n: Int): Queue[Int] = {
+    var ad = Queue[Int]()
+    for (i <- 0 until n) {
+      ad = ad :+ i
+    }
+    ad
+  }
   def saveThePrisoner(n: Int, m: Int, s: Int): Int = {
+    val rotations = (s - 1 + m) % n
+    getLast(rotations, n)
+  }
+  def saveThePrisoner3(n: Int, m: Int, s: Int): Int = {
+    var queue = fillQueue(n)
+    val rotations = (s - 1 + m) % n
+    for(i <- 0 until rotations){
+      queue = feed(queue)
+    }
+    queue.last + 1
+  }
+  def saveThePrisoner2(n: Int, m: Int, s: Int): Int = {
     val accountForMultipleRounds = m % n
     var ad = Queue[Int]()
     for (i <- 0 until n) {
