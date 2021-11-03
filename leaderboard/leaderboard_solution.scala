4169896c11fa9ea8eff13ce3d5e7dd911a94c54b
Chad Crowe <ccrowe@ccrowe-mn3.linkedin.biz>
2021-11-03 07:59:26 -0700

    cleaning up

--- leaderboard/leaderboard_solution.scala
+++ leaderboard/leaderboard_solution.scala
@@ -3,20 +3,20 @@ import scala.collection.SortedSet
 import java.util.Collections
 object Result {
 
-    /*
-     * Complete the 'climbingLeaderboard' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     *  1. INTEGER_ARRAY ranked
-     *  2. INTEGER_ARRAY player
-     */
+  /*
+   * Complete the 'climbingLeaderboard' function below.
+   *
+   * The function is expected to return an INTEGER_ARRAY.
+   * The function accepts following parameters:
+   *  1. INTEGER_ARRAY ranked
+   *  2. INTEGER_ARRAY player
+   */
 
   def getPrecedingScore2(newScore: Int, sortedKeys: SortedSet[Int]): Int = {
-    var lowestSoFar = sortedKeys.head    
-    for(key <- sortedKeys){
-      if(key <= newScore){
-        if(key > lowestSoFar){
+    var lowestSoFar = sortedKeys.head
+    for (key <- sortedKeys) {
+      if (key <= newScore) {
+        if (key > lowestSoFar) {
           lowestSoFar = key
         }
       }
@@ -28,16 +28,16 @@ object Result {
     var high = sortedKeys.size - 1
     var low = 0
     var lowestSoFar = sortedKeys.head
-    while(low <= high && low >= 0 && high < sortedKeys.size){
+    while (low <= high && low >= 0 && high < sortedKeys.size) {
       var mid = low + (high - low) / 2
       val value = sortedKeys(mid)
-      if(value.equals(newScore)){
+      if (value.equals(newScore)) {
         return value
       }
-      if(newScore > value){
+      if (newScore > value) {
         lowestSoFar = value
         low = mid + 1
-      }else{
+      } else {
         high = mid - 1
       }
     }
@@ -46,54 +46,64 @@ object Result {
   def processScores(ranked: Array[Int]): SortedMap[Int, Int] = {
     var sortedScores = SortedMap[Int, Int]()
     var currentRank = 0
-    for(rank <- ranked){
-      if(!sortedScores.contains(rank)){
+    for (rank <- ranked) {
+      if (!sortedScores.contains(rank)) {
         currentRank = currentRank + 1
       }
       sortedScores = sortedScores + (rank -> currentRank)
     }
-  sortedScores
+    sortedScores
   }
   def getAccumulation(newScore: Int, accumulations: Array[Int]): Int = {
     var scoreDecrement = 0
-    for(accumulation <- accumulations){
-      if(newScore < accumulation){
+    for (accumulation <- accumulations) {
+      if (newScore < accumulation) {
         scoreDecrement = scoreDecrement + 1
       }
     }
     scoreDecrement
   }
   def getRank(newScore: Int, lowerScore: Int, lowerRank: Int): Int = {
-    if(newScore.equals(lowerScore)) return lowerRank
-    if(newScore > lowerScore) return lowerRank
-    if(newScore < lowerScore) return lowerRank + 1
+    if (newScore.equals(lowerScore)) return lowerRank
+    if (newScore > lowerScore) return lowerRank
+    if (newScore < lowerScore) return lowerRank + 1
     throw new Exception("No rank found")
   }
-  def getPlayerScoreRank(newScore: Int, sortedScores: SortedMap[Int, Int], accumulations: Array[Int]): (Int, SortedMap[Int, Int], Array[Int]) = {
+  def getPlayerScoreRank(
+      newScore: Int,
+      sortedScores: SortedMap[Int, Int],
+      accumulations: Array[Int]
+  ): (Int, SortedMap[Int, Int], Array[Int]) = {
     val decrementFinalRankBy = getAccumulation(newScore, accumulations)
     val lowerScoreBy = getAccumulation(newScore, accumulations)
-    val existingLowerOrEqualScore: Int = getPrecedingScore2(newScore, sortedScores.keySet)
+    val existingLowerOrEqualScore: Int =
+      getPrecedingScore2(newScore, sortedScores.keySet)
     val oldRank = sortedScores(existingLowerOrEqualScore)
     val newScoreRank = getRank(newScore, existingLowerOrEqualScore, oldRank)
     val newTreeMap = sortedScores + (newScore -> newScoreRank)
-    return (newScoreRank + decrementFinalRankBy, newTreeMap, accumulations :+ newScore)
+    return (
+      newScoreRank + decrementFinalRankBy,
+      newTreeMap,
+      accumulations :+ newScore
+    )
   }
-    def climbingLeaderboard(ranked: Array[Int], player: Array[Int]): Array[Int] = {
-      var sortedScores = processScores(ranked)
-      var accumulations = Array[Int]()
-      var answers = Array[Int]()
-      for(playerScoreIndex <- 0 until player.size){
-        val playerScore = player(playerScoreIndex)
-        val currentPlayerRankData = getPlayerScoreRank(playerScore, sortedScores, accumulations)
-        val rank = currentPlayerRankData._1
-        answers = answers :+ rank
-        sortedScores = currentPlayerRankData._2
-        accumulations = currentPlayerRankData._3
-      }
-      answers
+  def climbingLeaderboard(
+      ranked: Array[Int],
+      player: Array[Int]
+  ): Array[Int] = {
+    var sortedScores = processScores(ranked)
+    var accumulations = Array[Int]()
+    var answers = Array[Int]()
+    for (playerScoreIndex <- 0 until player.size) {
+      val playerScore = player(playerScoreIndex)
+      val currentPlayerRankData =
+        getPlayerScoreRank(playerScore, sortedScores, accumulations)
+      val rank = currentPlayerRankData._1
+      answers = answers :+ rank
+      sortedScores = currentPlayerRankData._2
+      accumulations = currentPlayerRankData._3
     }
+    answers
+  }
 
 }
-
-
-
